473b2a739fbbaea8d010c5195317a6cf1ab1959b
woub21 <78636036+woub21@users.noreply.github.com>
2023-06-26 15:29:02 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,15 @@
+import sys
+
+if len(sys.argv) > 1:
+    name = sys.argv[1]
+else:
+    print("You are expected to provide your name.")
+    print("You need to type:")
+    print(f"\t python {sys.argv[0]} your_name")
+    sys.exit()
+print(f"My name is {name}.")
+print()
+
 print("Hello World!")
 print("\t This is my first Git repository on GitHub.")
 def print_message(name='Python'):
